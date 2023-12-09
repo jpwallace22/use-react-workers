0e2371b5aa55f5267a667fc7c3fdacd2d8ee988f
jpwallace22 <jpwallace22@gmail.com>
2023-12-09 10:00:36 -0800

    build: all one job

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,7 +7,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jpwallace22.github.io',
-  base: '/docs',
   integrations: [
     starlight({
       title: 'Use React Workers',
