340f4c4990570e9c54806d2f35606cb27d0287aa
jpwallace22 <jpwallace22@gmail.com>
2023-12-09 12:59:38 -0800

    fix: new base

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jpwallace22.github.io',
-  base: '/docs/',
+  base: '/use-react-workers/',
   outDir: 'build',
   integrations: [
     starlight({
