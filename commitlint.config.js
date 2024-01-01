96c2e694636ddcf5b7d9ba7cedd8ae167deb7d4a
Justin Wallace <jpwallace22@gmail.com>
2024-01-01 14:15:33 -0800

    ci: workflow for pr title (#14)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  // ignores [skip ci] so semantic-release can leave long commit messages
   ignores: [message => message.startsWith('WIP'), message => /\[skip ci\]/m.test(message)],
 };
